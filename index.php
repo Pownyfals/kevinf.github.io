b83d04089c49d6ac4c4bc8a9f615e24179269290
Pownyfals <38776020+Pownyfals@users.noreply.github.com>
2020-03-17 13:57:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+    <!-- My Css -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Portfolio - Kevin Febrian</title>
+</head>
+
+<body id="home" class="scrollspy">
+    <!-- Navbar -->
+    <div class="navbar-fixed">
+        <nav class="teal">
+            <div class="container">
+                <div class="nav-wrapper">
+                    <a href="#home" class="brand-logo">Kevin Febrian</a>
+                    <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">Menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="#skill">Skill</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+    <!-- slider -->
+    <div class="slider">
+        <ul class="slides">
+            <li>
+                <img src="img/portfolio/3.png">
+                <div class="caption left-align">
+                    <h3>Kevin Febrian</h3>
+                    <h5 class="light grey-text text-lighten-3">Welcome To My Portfolio</h5>
+                </div>
+            </li>
+            <li>
+                <img src="img/portfolio/2.png">
+                <div class="caption right-align">
+                    <h3>Kevin Febrian</h3>
+                    <h5 class="light grey-text text-lighten-3">Welcome To My Portfolio</h5>
+                </div>
+            </li>
+            <li>
+                <img src="img/portfolio/1.png">
+                <div class="caption center-align">
+                    <h3>Kevin Febrian</h3>
+                    <h5 class="light grey-text text-lighten-3">Welcome To My Portfolio</h5>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+   
+
+
+    <!-- Skill -->
+    <section id="skill" class="services grey lighten-3 scrollspy">
+        <div class="container">
+            <div class="row">
+
+                <h3 class="light center grey-text text-darken-3">Skills</h3>
+                <h5 class="center grey-text">An Explanation About My Skills</h5 class="center grey-text">
+                <div class="col m2 s12">
+                    <div class="card-panel center">
+                        <i class="material-icons medium">laptop_windows</i>
+                        <h5>HTML</h5>
+                        <p>90%
+                        </p>
+                    </div>
+                </div>
+                <div class="col m2 s12">
+                    <div class="card-panel center">
+                        <i class="material-icons medium">laptop_windows</i>
+                        <h5>PHP</h5>
+                        <p>80%
+
+                        </p>
+                    </div>
+                </div>
+                <div class="col m2 s12">
+                    <div class="card-panel center">
+                        <i class="material-icons medium">laptop_windows</i>
+                        <h5>Java</h5>
+                        <p>60%
+                        </p>
+                    </div>
+                </div>
+                <div class="col m2 s12">
+                    <div class="card-panel center">
+                        <i class="material-icons medium">laptop_windows</i>
+                        <h5>C#</h5>
+                        <p>70%
+                        </p>
+                    </div>
+                </div>
+                <div class="col m2 s12">
+                    <div class="card-panel center">
+                        <i class="material-icons medium">laptop_windows</i>
+                        <h5>C++</h5>
+                        <p>60%
+                        </p>
+                    </div>
+                </div>
+                <div class="col m2 s12">
+                    <div class="card-panel center">
+                        <i class="material-icons medium">laptop_windows</i>
+                        <h5>JS</h5>
+                        <p>60%
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Portfolio -->
+    <section id="portfolio" class="portfolio scrollspy">
+        <div class="container">
+            <h3 class="light center grey-text text-darken-3">Portfolio</h3>
+            <div class="row">
+                <div class="col m4 s12"> 
+                <h5 class="center grey-text">Web Company Profile</h5>
+                </div>
+                <div class="col m4 s12">
+                <h5 class="center grey-text">Profile</h5>
+                </div>
+                <div class="col m4 s12">
+                <h5 class="center grey-text"> Perpustakaan</h5>
+                </div>
+                <div class="row">
+                <div class="col m4 s12"> 
+                <h5 class="center">N/A</h5>
+                </div>
+                <div class="col m4 s12">
+                <h5 class="center">N/A</h5>
+                </div>
+                <div class="col m4 s12">
+                <h5 class="center"> N/A</h5>
+                </div>
+                    
+    </section>
+
+    <!-- contact us -->
+
+<div class="section" id="contact" class="contact scrollspy">
+<footer class="page-footer teal">
+    <div class="container">
+      <div class="row">
+
+
+        <div class="col m4 s12">
+        <div class="row">
+    <div class="col s12 m10">
+      <div class="card blue-grey darken-1 center">
+        <div class="card-content white-text center">
+          <span class="card-title center">Email</span>
+          <i class="material-icons small">contact_mail</i>
+          <p>pownyfals@gmail.com</p>
+        </div>
+        <div class="card-action">
+          <P></P>
+        </div>
+      </div>
+    </div>
+  </div>
+
+        </div>
+        <div class="col m4 s12">
+        <div class="row">
+    <div class="col s12 m10">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text center">
+          <span class="card-title">Phone</span>
+          <i class="material-icons small">contact_phone</i>
+          <p>087123456</p>
+        </div>
+        <div class="card-action">
+ 
+          
+        </div>
+      </div>
+    </div>
+  </div>
+        </div>
+        <div class="col m4 s12">
+        <div class="row">
+    <div class="col s12 m10">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text center">
+          <span class="card-title">Address</span>
+          <i class="material-icons small">location_on</i>
+          <p>West Java, Bandung</p>
+        </div>
+        <div class="card-action">
+          
+      </div>
+    </div>
+  </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="">Kevin Febrian</a>
+      </div>
+    </div>
+  </footer>
+
+
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script>
+            const sideNav = document.querySelectorAll('.sidenav');
+            M.Sidenav.init(sideNav);
+            const slider = document.querySelectorAll('.slider');
+            M.Slider.init(slider, {
+                indicators: false,
+                height: 500,
+                transition: 600,
+                interval: 4000
+            });
+
+            const materialbox = document.querySelectorAll('.materialboxed');
+            M.Materialbox.init(materialbox)
+
+            const parallax = document.querySelectorAll('.parallax');
+            M.Parallax.init(parallax);
+
+            const scroll = document.querySelectorAll('.scrollspy');
+            M.ScrollSpy.init(scroll, {
+                scrollOffset: 50
+            });
+        </script>
+</body>
+
+</html>
